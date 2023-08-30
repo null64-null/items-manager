747d9c6d6ebbb214d0d8b89d3590547cd4a97607
null64-null <hay.tech64@gmail.com>
2023-08-30 18:26:42 +0900

    add new page route

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ import 'ui/pages/hikidashi_list_page.dart';
 import './ui/pages/item_detail_page.dart';
 
 void main() {
-  int num = 2;
+  int num = 1;
   late dynamic page;
   if (num == 1) {
     page = const StartPage();
   } else if (num == 2) {
     page = const HikidashiListPage();
+  } else if (num == 3) {
+    page = const ShoppingPlaceListPage();
   }
   runApp(MaterialApp(home: page));
 }
