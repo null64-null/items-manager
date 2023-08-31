ecb311708f88b920c5d69999daeb159b928c683d
null64-null <hay.tech64@gmail.com>
2023-08-31 12:13:12 +0900

    delete unused file import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './ui/pages/start_page.dart';
 import './ui/pages/shopping_place_list_page.dart';
 import 'ui/pages/hikidashi_list_page.dart';
-import './ui/pages/item_detail_page.dart';
 
 void main() {
   int num = 1;
