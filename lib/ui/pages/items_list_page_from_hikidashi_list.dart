a7753af105ef8ddf8a5a1889ad86a73c9e680cb0
null64-null <hay.tech64@gmail.com>
2023-09-05 19:52:48 +0900

    make item list page from hikidashi

---
+++ lib/ui/pages/items_list_page_from_hikidashi_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemListPageFromHikidashiList extends StatelessWidget {
+  const ItemListPageFromHikidashiList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
