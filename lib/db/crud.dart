d7570c31145ee7684e1ab6ed04fc066a7ca2eb97
null64-null <hay.tech64@gmail.com>
2023-09-27 17:58:26 +0900

    make all crud in hikidashi

--- lib/db/crud.dart
+++ lib/db/crud.dart
@@ -3,6 +3,8 @@ import './models.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+// Hikidashi
+// create
 Future<void> insertHikidashi(Hikidashi hikidashi) async {
   final Database db = await openDatabase(
     join(await getDatabasesPath(), 'zaiko_databases.db'),
@@ -10,17 +12,53 @@ Future<void> insertHikidashi(Hikidashi hikidashi) async {
   await db.insert('hikidashis', hikidashi.toMap());
 }
 
+// get (all)
 Future<List<Hikidashi>> getHikidashi() async {
-  debugPrint("get start");
   final Database db = await openDatabase(
     join(await getDatabasesPath(), 'zaiko_databases.db'),
   );
   final List<Map<String, dynamic>> maps = await db.query('hikidashis');
-  debugPrint(maps.toString());
   return List.generate(maps.length, (index) {
     return Hikidashi(
       id: maps[index]['id'],
       name: maps[index]['name'],
     );
   });
+}
+
+// edit
+Future<void> updateHikidashi(Hikidashi hikidashi) async {
+  final Database db = await openDatabase(
+    join(await getDatabasesPath(), 'zaiko_databases.db'),
+  );
+  await db.update(
+    'zaiko_databases.db',
+    hikidashi.toMap(),
+    where: 'id = ?',
+    whereArgs: [hikidashi.id],
+  );
+}
+
+// delete
+Future<void> deleteHikidashi(int id) async {
+  final Database db = await openDatabase(
+    join(await getDatabasesPath(), 'zaiko_databases.db'),
+  );
+  await db.delete(
+    'zaiko_databases.db',
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+}
+
+// delete (select)
+Future<void> deleteHikidashis(List<int> ids) async {
+  final Database db = await openDatabase(
+    join(await getDatabasesPath(), 'zaiko_databases.db'),
+  );
+  await db.delete(
+    'zaiko_databases.db',
+    where: 'id IN (${ids.map((_) => '?').join(', ')})',
+    whereArgs: ids,
+  );
 }
